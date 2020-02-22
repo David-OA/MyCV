d8cc2de0a26af22151b828bbff3af5bb281f9752
David.OA <david.oconte@orange.fr>
2020-02-22 14:53:17 +0100

    I add some style =).

--- app/src/main/java/com/oconte/david/mycurriculumvitae/Fragments/CoordonneesFragment.java
+++ app/src/main/java/com/oconte/david/mycurriculumvitae/Fragments/CoordonneesFragment.java
@@ -1,14 +1,21 @@
 package com.oconte.david.mycurriculumvitae.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.oconte.david.mycurriculumvitae.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CoordonneesFragment extends Fragment {
+
 
     public static CoordonneesFragment newInstance() {
         return (new CoordonneesFragment());
@@ -17,5 +24,9 @@ public class CoordonneesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_coordonnees, container, false);
+
+
+
     }
+
 }
