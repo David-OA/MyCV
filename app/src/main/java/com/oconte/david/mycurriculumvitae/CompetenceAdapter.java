c3fbfd6241b94af77b78f44c4723be5815564001
David.OA <david.oconte@orange.fr>
2020-05-14 15:15:09 +0200

    I add recyclerView for all competence I try to make this more nice to see it.

---
+++ app/src/main/java/com/oconte/david/mycurriculumvitae/CompetenceAdapter.java
@@ -0,0 +1,54 @@
+package com.oconte.david.mycurriculumvitae;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CompetenceAdapter extends RecyclerView.Adapter<CompetenceAdapter.CompetenceViewHolder>{
+
+    private ArrayList<CompetenceItem> CompetenceList;
+
+    public static class CompetenceViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+
+        public CompetenceViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public CompetenceAdapter(ArrayList<CompetenceItem> competenceList) {
+        CompetenceList = competenceList;
+    }
+
+    @Override
+    public CompetenceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.competence_item, parent, false);
+        CompetenceViewHolder evh = new CompetenceViewHolder(v);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(CompetenceViewHolder holder, int position) {
+        CompetenceItem currentItem = CompetenceList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return CompetenceList.size();
+    }
+}
--- app/src/main/java/com/oconte/david/mycurriculumvitae/Fragments/HomeFragment.java
+++ app/src/main/java/com/oconte/david/mycurriculumvitae/Fragments/HomeFragment.java
@@ -2,11 +2,20 @@ package com.oconte.david.mycurriculumvitae.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.oconte.david.mycurriculumvitae.CompetenceAdapter;
+import com.oconte.david.mycurriculumvitae.CompetenceItem;
 import com.oconte.david.mycurriculumvitae.R;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeFragment extends Fragment {
 
@@ -14,8 +23,27 @@ public class HomeFragment extends Fragment {
         return (new HomeFragment());
     }
 
+    @BindView (R.id.recyclerView) RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+
+
+            ArrayList<CompetenceItem> competenceList = new ArrayList<>();
+            competenceList.add(new CompetenceItem(R.drawable.david26, "Line 1", "Line 2"));
+
+            mRecyclerView.setHasFixedSize(true);
+            mLayoutManager = new LinearLayoutManager(getContext());
+            mAdapter = new CompetenceAdapter(competenceList);
+
+            mRecyclerView.setLayoutManager(mLayoutManager);
+            mRecyclerView.setAdapter(mAdapter);
+
+        return view;
+
     }
 }
