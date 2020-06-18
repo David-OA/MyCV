62ea0314ca953c8402bd67fc540c7420a17c504c
David.OA <david.oconte@orange.fr>
2020-06-18 16:22:26 +0200

    I change my recyclerView for most style. It's not finish.

--- app/src/main/java/com/oconte/david/mycurriculumvitae/Fragments/HomeFragment.java
+++ app/src/main/java/com/oconte/david/mycurriculumvitae/Fragments/HomeFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.oconte.david.mycurriculumvitae.RecyclerView1.CompetenceAdapter;
+import com.oconte.david.mycurriculumvitae.RecyclerView.CompetenceAdapter;
 import com.oconte.david.mycurriculumvitae.CompetenceItem;
 import com.oconte.david.mycurriculumvitae.R;
 
@@ -24,8 +24,7 @@ public class HomeFragment extends Fragment {
     }
 
     @BindView (R.id.recyclerView) RecyclerView mRecyclerView;
-    @BindView (R.id.recyclerView2) RecyclerView mRecyclerView2;
-    @BindView (R.id.recyclerView3) RecyclerView mRecyclerView3;
+
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
@@ -34,71 +33,57 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, view);
 
+        this.recyclerViewSetting();
 
-            ArrayList<CompetenceItem> competenceList = new ArrayList<>();
-            competenceList.add(new CompetenceItem(R.drawable.android_studio, "Android Studio", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.github, "Git et GitHub", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.drupal, "Drupal 7 et 8", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.html5_css3, "HTML5 et CSS3", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.java, "Java", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.symfony, "Symfony", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.php_mysql, "PHP et MySQL", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.canva, "Canva", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.gimp, "Gimp", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.david26, "SIG : Cart@Jour, mapinfo", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.david26, "SIG : Gérémi, Qgis", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.david26, "Saphir pour l'eau potable", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.autocad, "Autocad", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.microsoft_office, "La suite Microsoft", "Line 2"));
-            competenceList.add(new CompetenceItem(R.drawable.open_office, "Open Office", "Line 2"));
-
-            mRecyclerView.setHasFixedSize(true);
-            mLayoutManager = new LinearLayoutManager(getContext());
-            mAdapter = new CompetenceAdapter(competenceList);
-
-            mRecyclerView.setLayoutManager(mLayoutManager);
-            mRecyclerView.setAdapter(mAdapter);
-
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // ADMININISTRATIVE LIST
-
-        ArrayList<CompetenceItem> competenceList2 = new ArrayList<>();
-        competenceList2.add(new CompetenceItem(R.drawable.android_studio, "Site internet", "Line 2"));
-        competenceList2.add(new CompetenceItem(R.drawable.github, "Gestion d'une base de données en Biologie", "Line 2"));
-        competenceList2.add(new CompetenceItem(R.drawable.drupal, "Communication au près des usagers", "Line 2"));
-        competenceList2.add(new CompetenceItem(R.drawable.html5_css3, "Commissions avec des élus", "Line 2"));
-        competenceList2.add(new CompetenceItem(R.drawable.java, "Réunions publics et professionnelles", "Line 2"));
-        competenceList2.add(new CompetenceItem(R.drawable.symfony, "Rapports et collectes des données sur logiciels", "Line 2"));
-        competenceList2.add(new CompetenceItem(R.drawable.php_mysql, "Budget prévisionnel et facturation", "Line 2"));
-
-        mRecyclerView2.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(getContext());
-        mAdapter = new CompetenceAdapter(competenceList2);
-
-        mRecyclerView2.setLayoutManager(mLayoutManager);
-        mRecyclerView2.setAdapter(mAdapter);
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // TECHNIQUE LIST
-
-        ArrayList<CompetenceItem> competenceList3 = new ArrayList<>();
-        competenceList3.add(new CompetenceItem(R.drawable.android_studio, "intervention_et_suivi_sur_des_cas_sp_ciaux_ex_une_laiterie", "Line 2"));
-        competenceList3.add(new CompetenceItem(R.drawable.github, "Mesure et réglages en station et réservoir.", "Line 2"));
-        competenceList3.add(new CompetenceItem(R.drawable.drupal, "Suivi et contrôle de chantier", "Line 2"));
-        competenceList3.add(new CompetenceItem(R.drawable.html5_css3, "Chiffrage de travaux", "Line 2"));
-        competenceList3.add(new CompetenceItem(R.drawable.java, "Recherche et réparation de fuites sur réseau.", "Line 2"));
-        competenceList3.add(new CompetenceItem(R.drawable.symfony, "Mise en place de la télé relève: type sofrel", "Line 2"));
-        competenceList3.add(new CompetenceItem(R.drawable.php_mysql, "Participation aux groupes de travail, départementale, régionnale et nationnale.", "Line 2"));
-
-        mRecyclerView3.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(getContext());
-        mAdapter = new CompetenceAdapter(competenceList3);
-
-        mRecyclerView3.setLayoutManager(mLayoutManager);
-        mRecyclerView3.setAdapter(mAdapter);
 
         return view;
+    }
 
+    public void recyclerViewSetting() {
+        ArrayList<CompetenceItem> competenceList = new ArrayList<>();
+
+        //COMPETENCES
+
+        //Logicielles
+        competenceList.add(new CompetenceItem(R.drawable.android_studio, "Android Studio", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.github, "Git et GitHub", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.drupal, "Drupal 7 et 8", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.html5_css3, "HTML5 et CSS3", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.java, "Java", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.symfony, "Symfony", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.php_mysql, "PHP et MySQL", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.canva, "Canva", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.gimp, "Gimp", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.david26, "SIG : Cart@Jour, mapinfo", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.david26, "SIG : Gérémi, Qgis", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.david26, "Saphir pour l'eau potable", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.autocad, "Autocad", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.microsoft_office, "La suite Microsoft", "Logicielles"));
+        competenceList.add(new CompetenceItem(R.drawable.open_office, "Open Office", "Logicielles"));
+
+        //Administratives need to change pictures
+        competenceList.add(new CompetenceItem(R.drawable.android_studio, "Site internet", "Administratives"));
+        competenceList.add(new CompetenceItem(R.drawable.github, "Gestion d'une base de données en Biologie", "Administratives"));
+        competenceList.add(new CompetenceItem(R.drawable.drupal, "Communication au près des usagers", "Administratives"));
+        competenceList.add(new CompetenceItem(R.drawable.html5_css3, "Commissions avec des élus", "Administratives"));
+        competenceList.add(new CompetenceItem(R.drawable.java, "Réunions publics et professionnelles", "Administratives"));
+        competenceList.add(new CompetenceItem(R.drawable.symfony, "Rapports et collectes des données sur logiciels", "Administratives"));
+        competenceList.add(new CompetenceItem(R.drawable.php_mysql, "Budget prévisionnel et facturation", "Administratives"));
+
+        //Techniques need to change pictures
+        competenceList.add(new CompetenceItem(R.drawable.android_studio, "intervention_et_suivi_sur_des_cas_sp_ciaux_ex_une_laiterie", "Techniques"));
+        competenceList.add(new CompetenceItem(R.drawable.github, "Mesure et réglages en station et réservoir.", "Techniques"));
+        competenceList.add(new CompetenceItem(R.drawable.drupal, "Suivi et contrôle de chantier", "Techniques"));
+        competenceList.add(new CompetenceItem(R.drawable.html5_css3, "Chiffrage de travaux", "Techniques"));
+        competenceList.add(new CompetenceItem(R.drawable.java, "Recherche et réparation de fuites sur réseau.", "Techniques"));
+        competenceList.add(new CompetenceItem(R.drawable.symfony, "Mise en place de la télé relève: type sofrel", "Techniques"));
+        competenceList.add(new CompetenceItem(R.drawable.php_mysql, "Participation aux groupes de travail, départementale, régionnale et nationnale.", "Techniques"));
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new CompetenceAdapter(competenceList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/com/oconte/david/mycurriculumvitae/RecyclerView/CompetenceAdapter.java
+++ app/src/main/java/com/oconte/david/mycurriculumvitae/RecyclerView/CompetenceAdapter.java
@@ -1,4 +1,4 @@
-package com.oconte.david.mycurriculumvitae.RecyclerView1;
+package com.oconte.david.mycurriculumvitae.RecyclerView;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
