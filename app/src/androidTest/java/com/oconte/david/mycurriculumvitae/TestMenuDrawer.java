8c2f9e876144c4d8eefbe0dc0bed097796db00fe
DAVID-OA <david.oconte@orange.fr>
2020-10-16 15:36:10 +0200

    I start to make some test.

--- app/src/androidTest/java/com/oconte/david/mycurriculumvitae/TestMenuDrawer.java
+++ app/src/androidTest/java/com/oconte/david/mycurriculumvitae/TestMenuDrawer.java
@@ -1,5 +1,7 @@
 package com.oconte.david.mycurriculumvitae;
 
+import android.view.Gravity;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -31,20 +33,12 @@ public class TestMenuDrawer {
         //Start the MainActivity
         mActivityRule.launchActivity(null);
 
-        // Open Drawer to click on navigation.
-        //onView(withId(R.id.activity_main_drawerLayout))
-                //.check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
-                //.perform(DrawerActions.open()); // Open Drawer
-
         onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
-        /* Start the screen of your activity.
-        onView(withId(R.id.nav_view))
-                .perform(NavigationViewActions.navigateTo(R.id.your_navigation_menu_item));
 
-        / Check that you Activity was opened.
-        String expectedNoStatisticsText = InstrumentationRegistry.getTargetContext()
-                .getString(R.string.no_item_available);
-        onView(withId(R.id.no_statistics)).check(matches(withText(expectedNoStatisticsText)));*/
+        onView(withId(R.id.activity_main_nav_view)).check(matches(isDisplayed()));
+
+        // Open Drawer to click on navigation.
+        //onView(withId(R.id.activity_main_drawer_coordonnees)).check(matches(isDisplayed()));
 
     }
 }
