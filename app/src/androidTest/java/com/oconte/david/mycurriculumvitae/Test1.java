5a20e3baa3a8c06e14db88e557f433255f352dc6
DAVID-OA <david.oconte@orange.fr>
2020-10-16 11:38:28 +0200

    I start to make some test.

--- app/src/androidTest/java/com/oconte/david/mycurriculumvitae/Test1.java
+++ app/src/androidTest/java/com/oconte/david/mycurriculumvitae/Test1.java
@@ -14,16 +14,11 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-public class TestMenuDrawer {
-
+public class Test1 {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class, false, false);
+
 
     @Test
     public void testCoordonnées() throws IOException, InterruptedException {
@@ -33,8 +28,8 @@ public class TestMenuDrawer {
 
         // Open Drawer to click on navigation.
         //onView(withId(R.id.activity_main_drawerLayout))
-                //.check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
-                //.perform(DrawerActions.open()); // Open Drawer
+        //.check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+        //.perform(DrawerActions.open()); // Open Drawer
 
         onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
         /* Start the screen of your activity.
